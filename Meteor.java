ab454d9abb183bf81867dfea5e7a3b0d016753ea
AidenS588 <335674172@gapps.yrdsb.ca>
2022-06-10 14:14:47 -0400

    Day 11 coding

---
+++ Meteor.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Meteor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Meteor extends Actor
+{
+    /**
+     * Act - do whatever the Meteor wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    double gravity = 0;
+    int terminalVelocity = 10;
+    public void act()
+    {
+        gravity += 0.5;
+        if(gravity == terminalVelocity)
+        {
+            gravity = terminalVelocity;
+        }
+        if(getY() >= 399 || isTouching(Terry.class))
+        {
+            gravity = 0; 
+        }
+        setLocation(getX(), getY() + (int)gravity);
+        // Add your action code here.
+        if(getY() >= 399)
+        {
+             getWorld().removeObject(this);
+             getWorld().removeObjects(getWorld().getObjects(Ruby.class));
+        }
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -15,8 +15,17 @@ public class MyWorld extends World
      */
     private Terry terry;
     public int lifePoints = 2;
+    
+    public int impactFrame = 600;
+    public int impactTimer = impactFrame/60;
+    
+    public int waveCounter = 1;
+    
     Label lifeLabel;
+    Label meteorTimer;
+    Label wave;
     private int frame = 0;
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -24,14 +33,20 @@ public class MyWorld extends World
         
         // Create Terry the Pterodactyl 
         terry = new Terry();
-        addObject(terry, 300, 200);
+        addObject(terry, 600, 200);
         
         //Create the rock
         Rock rock = new Rock(terry);
-        addObject(rock, 30, 200);
+        addObject(rock, 600, 400);
         
         lifeLabel = new Label(lifePoints, 40);
         addObject(lifeLabel, 1150, 30);
+        
+        meteorTimer = new Label(impactTimer, 40);
+        addObject(meteorTimer, 1100, 30);
+        
+        wave = new Label(waveCounter, 40);
+        addObject(wave, 1050, 30);
     }
     
     public void decreaseLife()
@@ -44,11 +59,29 @@ public class MyWorld extends World
     {
         frame++;
         //Create Rubys
+        if(frame % 60 == 0)
+        {
+            impactTimer--;
+            if(impactTimer <= 0)
+            {
+                impactTimer = impactFrame/60;
+            }
+            meteorTimer.setValue(impactTimer);
+        }
+        
         if(frame % 120 == 0)
         {
             Ruby ruby = new Ruby(terry);
             addObject(ruby, Greenfoot.getRandomNumber(1200), 750);
         }
         
+        if(frame % impactFrame == 0)
+        {
+            Meteor meteor = new Meteor();
+            addObject(meteor, Greenfoot.getRandomNumber(1200), 0);
+            impactFrame += impactFrame;
+            waveCounter++;
+            wave.setValue(waveCounter);
+        }
     }
 }
